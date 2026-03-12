62d6eb7cbd8c95eb99e4ebe92c6cd5b76178a1fe
Mouaad <aitsaidmouaad.dev@gmail.com>
2026-03-12 04:35:23 +0100

    test deploy

--- apps/test-deploy/src/app/app.controller.ts
+++ apps/test-deploy/src/app/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports -- NestJS DI requires value import for decorator metadata
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports -- NestJS DI requires  value import for decorator metadata
 import { AppService } from './app.service';
 
 @Controller()
